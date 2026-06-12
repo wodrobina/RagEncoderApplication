a6a23c26fd07cfa2398e4f7f6c6f19ee92a9158b
Wojciech Odrobina <wodrobina@epages.com>
2026-06-12 13:03:46 +0200

    Use QdrantVectorStore

--- src/main/java/eu/wodrobina/ragencoderapplication/index/RagConfig.java
+++ src/main/java/eu/wodrobina/ragencoderapplication/index/RagConfig.java
@@ -1,13 +1,16 @@
 package eu.wodrobina.ragencoderapplication.index;
 
+import eu.wodrobina.ragencoderapplication.config.QdrantProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RagConfig {
 
     @Bean
-    public VectorStore vectorStore() {
-        return new InMemoryVectorStore();
+    public VectorStore vectorStore(RestClient.Builder builder, QdrantProperties properties) {
+        return new QdrantVectorStore(builder, properties);
     }
+
 }
